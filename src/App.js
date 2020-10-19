871f899f563801f3f54640c6bea0b503dbc33980
hackape <v@hackape.com>
2020-10-19 14:03:58 +0800

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,49 +1,97 @@
-import React, { useState } from 'react'
-import { doubleToIEEE, calcExponent } from './utils'
+// @ts-check
+import React, { useState, useMemo } from "react";
+import { Float64ArrayView, calcExponent } from "./utils";
 
 export default function App() {
-  const [num, setNum] = useState(0)
-  
-  function calculateNumValue(value) {
-    try {
-      var v = eval(value)
-      if (typeof v === 'number') setNum(v)
-    } catch (e) {}
-  }
-  
+  const setVersion = useState(0)[1];
+
+  const float64view = useMemo(() => {
+    const float64view = new Float64ArrayView(setVersion);
+    return float64view;
+  }, []);
+
+  const calculateNumValue = useMemo(() => {
+    function calculateNumValue(value) {
+      try {
+        var v = +eval(value);
+        if (!Number.isNaN(v)) {
+          float64view.setValue(v);
+        }
+      } catch (e) {}
+    }
+
+    return calculateNumValue;
+  }, [float64view]);
+
+  const binary64 = float64view.getBinary64();
+
+  const { sign, exponent, significand } = binary64;
+
   return (
-    <div>
-      <div>input: <input onChange={e => calculateNumValue(e.target.value)} /></div>
-      <div>number: {num}</div>
-      <Bits bits={doubleToIEEE(num)} />
+    <div className="App">
+      <h1>IEEE-754 Float64 Converter</h1>
+      <div className="InputContainer">
+        Input:{" "}
+        <input
+          className="InputBox"
+          onChange={(e) => calculateNumValue(e.target.value)}
+        />
+      </div>
+      <div className="ViewContainer">
+        <div>Decimal: {float64view.getValue()}</div>
+        <div>
+          Binary: {sign} {exponent} {significand}
+        </div>
+        <Binary64
+          binary64={binary64}
+          toggleBit={(pos) => float64view.toggleBit(pos)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-function Bits({ bits }) {
-  const sign = bits.slice(0, 1)
-  const exponent = bits.slice(1, 12)
-  const mantissa = bits.slice(12)
+function Binary64({ binary64, toggleBit }) {
+  const { exponent, sign, significand } = binary64;
   return (
-    <div className='Bits'>
-      <div className='Sign'>
-        <div>sign</div>
-        <div><BitRow bits={sign} /></div>
+    <div className="Binary64">
+      <div className="Sign BitsFragment">
+        <div>Sign</div>
+        <Bits bits={sign} offset={0} toggleBit={toggleBit} />
       </div>
-      <div className='Exponent'>
-        <div>exp</div>
-        <div><BitRow bits={exponent} /></div>
-        <div>2<sup>{calcExponent(exponent)}</sup></div>
+      <div className="Exponent BitsFragment">
+        <div>Exponent</div>
+        <Bits bits={exponent} offset={1} toggleBit={toggleBit} />
+        <div>
+          2<sup>{calcExponent(exponent)}</sup>
+        </div>
       </div>
-      <div className='Mantissa'>
-        <div>mantissa</div>
-        <div><BitRow bits={mantissa} /></div>
-
+      <div className="Significand BitsFragment">
+        <div>Significand</div>
+        <Bits bits={significand} offset={12} toggleBit={toggleBit} />
       </div>
     </div>
-  )
+  );
 }
 
-function BitRow({ bits }) {
-  return bits.map((d, i) => <div key={i} className='bit'>{d}</div>)
+function Bits({ bits, offset, toggleBit }) {
+  return (
+    <div className="Bits">
+      {bits.map((bit, i) => {
+        const index = offset + i;
+        return (
+          <div
+            key={index}
+            className="Bit"
+            onClick={() => {
+              toggleBit(63 - index);
+            }}
+          >
+            <sub>{63 - index}</sub>
+            <div>{bit}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,52 @@
-export function doubleToIEEE(num) {
-  const buf = new ArrayBuffer(8)
-  const f = new Float64Array(buf)
-  f[0] = num
-  const uint = new Uint32Array(buf)
-  return `${uint[1].toString(2).padStart(32, '0')}${uint[0].toString(2).padStart(32, '0')}`.split('').map(v => +v)
+export class Float64ArrayView {
+  constructor(setVersion) {
+    this.buffer = new ArrayBuffer(8);
+    this.float = new Float64Array(this.buffer);
+    this.uint8 = new Uint8Array(this.buffer);
+    this.uint32 = new Uint32Array(this.buffer);
+    this.update = () => {
+      setVersion((v) => v + 1);
+    };
+  }
+
+  toggleBit(pos) {
+    const index = pos % 8;
+    const uintIndex = (pos - index) / 8;
+    this.uint8[uintIndex] ^= 1 << index;
+    this.update();
+  }
+
+  getBinary64() {
+    const uint = this.uint32;
+
+    const bits = `${uint[1].toString(2).padStart(32, "0")}${uint[0]
+      .toString(2)
+      .padStart(32, "0")}`
+      .split("")
+      .map((v) => +v);
+
+    const sign = bits.slice(0, 1);
+    const exponent = bits.slice(1, 12);
+    const significand = bits.slice(12);
+    return {
+      bits,
+      sign,
+      exponent,
+      significand,
+    };
+  }
+
+  setValue(val) {
+    this.float[0] = val;
+    this.update();
+  }
+
+  getValue() {
+    return this.float[0];
+  }
 }
 
 export function calcExponent(expBits) {
-  const bias = 1023  // for 11-bit exp. formula: 2**(bits-1) - 1
-  return parseInt(expBits.join(''), 2) - bias
+  const bias = 1023; // for 11-bit exp. formula: 2**(bits-1) - 1
+  return parseInt(expBits.join(""), 2) - bias;
 }
